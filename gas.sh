78cd6e9731454ab24c0567d1795ab901c13f0e10
GALIRUS404 <galirusofficial@gmail.com>
2024-09-14 23:19:55 +0700

    Update gas.sh

--- gas.sh
+++ gas.sh
@@ -3,11 +3,7 @@ file="node_modules"
 if [ -d "$file" ]; then
     while true; do
         clear
-        play -q $HOME/TOOLSV5/sound/robot2.mp3 &>/dev/null
-        echo "BOT DI JALANKAN 24 JAM UNTUK BERHENTI CTRL+C"
-        echo "TUNGGU 10 DETIK"
-        sleep 10
-        play -q $HOME/TOOLSV5/sound/klik.mp3 &>/dev/null
+        play -q $HOME/Lubeban/sound/klik.mp3 &>/dev/null
         echo "Apakah Anda ingin menghapus session sebelumnya? (y/n)"
         read -p "Pilihan Anda: " hapus_session
         if [ "$hapus_session" = "y" ] || [ "$hapus_session" = "Y" ]; then
@@ -21,7 +17,7 @@ if [ -d "$file" ]; then
     done
 else
     clear
-    play -q $HOME/TOOLSV5/sound/salah.mp3 &>/dev/null
+    play -q $HOME/Lubeban/sound/salah.mp3 &>/dev/null
     echo "Node_Modules Belum Terinstall"
     sleep 5
     clear
